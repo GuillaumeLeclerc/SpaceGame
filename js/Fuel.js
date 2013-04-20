5f2ee85bb766f1a701fde133cbb33cab1b0eca1d
George Christodoulou <giorkosman@gmail.com>
2013-04-20 16:25:44 +0200

    Fuel Finished

--- js/Fuel.js
+++ js/Fuel.js
@@ -1,7 +1,26 @@
 ﻿/// <reference path="Point.js" />
+/**/
 
-function Fuel() {
+
+function Fuel(amount_,fuelEfficiency_,mass_value_) {
     var that = this;
-    x = new Point(1, 1);
+    that.amount = amount_;
+    that.massValue =  mass_value_;
+
+    /*that.fuelEfficiency = fuelEfficiency_;
+    */
+    that.fuelEfficiency = 1;
+
+
+    that.getEnergy = function (fuelConsumption_) {
+        that.amount -= fuelConsumption_;
+        return that.fuelEfficiency * fuelConsumption_;
+    }
+
+    that.getMass = function () {
+        return that.mass_value_ * that.amount;
+    }
+
+
 
 }
