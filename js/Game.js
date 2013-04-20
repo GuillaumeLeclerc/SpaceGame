0f477114b0bc09dcf636f0031363b1643287a48c
Guillaume Leclerc <guillaume.leclerc.work@gmail.com>
2013-04-20 23:29:58 +0200

    game.js

--- js/Game.js
+++ js/Game.js
@@ -3,6 +3,7 @@
 /// <reference path="Entity.js" />
 /// <reference path="Fuel.js" />
 /// <reference path="Rocket.js" />
+/// <reference path="ImageProvider.js" />
 
 window.requestAnimFrame =
     (function () {
@@ -12,16 +13,44 @@ window.requestAnimFrame =
         window.oRequestAnimationFrame ||
         window.msRequestAnimationFrame ||
         function (callback, element) {
-            window.setTimeout(callback, 1000 / 30);
+            window.setTimeout(callback, 1000 / 30 , element);
         };
     }
     )();
 
-function Game() {
+function Game(canvasId , entryPoint) {
     var that= this;
 
-    that.data = {
+    var _fuelAmount=1;
+    var _fuelEfficiency=3312000;
+    var _fuelMassValue=1;
+    var _qsumption=0.9;
+    var _rocketMass=1000;
+    var _planetMass=5.97219E24;
+    var _planetRadius=6.371E6;
+    var myPlanet = new Planet(_planetMass, _planetRadius);
+    var myFuel = new Fuel(_fuelAmount, _fuelEfficiency, _fuelMassValue);
+    var myEngine = new Engine(_engineMass, myFuel, _engineEfficiency, _engineFuelConsumption);
+    var myRocket = new Rocket(_rocketMass, myEngine);
 
+    var mainCanvas = document.getElementById("mainCanvas");
+    mainCanvas.width = mainCanvas.clientWidth;
+    mainCanvas.height = mainCanvas.clientHeight;
+    var mainContext = mainCanvas.getContext("2d");
+
+    var imagesToLoad = {
+        rocket : "img/rocket"
+    };
+
+
+    that.data = {
+        rocket: myRocket,
+        fuel: myFuel,
+        engine: myEngine,
+        planet: myPlanet,
+        canvas: mainCanvas,
+        canvasContext: mainContext,
+        images: new Array()
     };
 
     that.presenters = {
@@ -30,6 +59,57 @@ function Game() {
         game: new GamePresenter(that),
         question : null 
     };
+
+    that.lastLoop = new Date().getMilliseconds();
+    that.currentPresenter = entryPoint;
+    that.runing = true;
+    that.firstPresenter = true;
+
+    that.getCurrentPresenter = function () {
+        return that.presenters[that.currentPresenter];
+    }
+
+    that.innerLoop = function()
+    {
+        var currentTime = new Date().getMilliseconds();
+        var timeSpan = that.lastLoop - currentTime;
+        if (that.firstPresenter) {
+            that.getCurrentPresenter().setUp();
+        }
+        var answer = that.presenters[that.currentPresenter].nextStep(timeSpan/1000);
+        if (answer != -1)
+        {
+            that.changePresenter(answer);
+        }
+        that.lastLoop = new Date().getMilliseconds();
+        if(that.runing)
+            requestAnimFrame(function () { that.innerLoop()});
+    }
+
+    that.changePresenter = function (nextPresenter) {
+
+    }
+
+    that.onPointerUp = function (evt) {
+        evt.preventDefault();
+        that.getCurrentPresenter().pointerUp(evt);
+    }
+
+    that.onPointerDown = function (evt) {
+        evt.preventDefault();
+        that.getCurrentPresenter().pointerDown(evt);
+    }
+
+    that.setUp = function(){
+        that.data.canvas.addEventListener("PointerDown", that.onPointerDown, false);
+        that.data.canvas.addEventListener("PointerUp", that.onPointerUp, false);
+    }
+
+    that.start = function () {
+        that.setUp();
+        that.lastLoop = new Date().getMilliseconds();
+        that.innerLoop();
+    }
 
 
 
---
+++ js/ImageProvider.js
@@ -0,0 +1,15 @@
+﻿
+
+
+function ImageProvider(iUrl , iCallback) {
+    var me = this;
+    this.Url = iUrl;
+    this.callback = iCallback;
+    this.Loaded = false;
+    this.base = new Image();
+    this.base.onload = function () {
+        me.Loaded = true;
+        me.callback();
+    }
+    this.base.src = iUrl;
+}
