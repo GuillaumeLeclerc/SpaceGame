2562f7c24f969b0bef6fd0b98513d4571b26452f
Guillaume Leclerc <guillaume.leclerc.work@gmail.com>
2013-04-20 17:00:37 +0200

    first planet

---
+++ js/Planet.js
@@ -0,0 +1,13 @@
+﻿function Planet(i_mass, i_radius) {
+    var that = this;
+    that.mass = i_mass;
+    that.radius = i_radius;
+
+    that.getGravity = function(o_mass , o_height){
+        return Constants.bigG * that.mass * o_mass / Math.pow(that.radius + o_height, 2);
+    }
+}
+
+window.Constants = {
+    bigG: 6.673E-11
+};
