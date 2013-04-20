b3be084c2f37ee0cb2584e0fde2320509d348e3d
Guillaume Leclerc <guillaume.leclerc.work@gmail.com>
2013-04-20 15:42:23 +0200

    add every operations on point

--- js/Point.js
+++ js/Point.js
@@ -8,13 +8,35 @@
         that.y += p.y;
     }
 
+    that.scalarAdd = function(c){
+        that.x += c;
+        that.y += c; 
+    }
+
     that.divide = function (p) {
-        that.x /= p.x;
-        that.y /= p.y;
+        if (p.x != 0) {
+            that.x /= p.x;
+        }
+        if (p.y != 0) {
+            that.y /= p.y;
+        }
     }
 
     that.scalarDivide = function (c) {
+        if (c == 0) {
+            return;
+        }
         that.x /= c;
         that.y /= c;
+    }
+
+    that.multiply = function(p){
+        that.x *= p.x;
+        that.y *= p.y; 
+    }
+
+    that.scalarMultiply = function (c) {
+        that.x *= c;
+        that.y *= c;
     }
 }
