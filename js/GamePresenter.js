9cb09722b19e12aaf9316365a81265fdc61c1d3a
Guillaume Leclerc <guillaume.leclerc.work@gmail.com>
2013-04-21 04:59:37 +0200

    hope

--- js/GamePresenter.js
+++ js/GamePresenter.js
@@ -1,3 +1,4 @@
+/// <reference path="Star.js" />
 // Render function 
 
 // images are in
@@ -43,11 +44,20 @@ function GamePresenter(parent)
 	that.context = parent.data.canvasContext;
 	that.width = parent.data.canvas.width;
 	that.height = parent.data.canvas.height;
+	that.backGround = new PRCanvas(that.width, that.height);
+	that.stars = new Array();
+	that.NBStars = 0;
+	that.MaxStars = 50;
+	that.clock = 0;
 	
 	
 	
 	that.setUp = function()
 	{
+	    that.backGround.Context.fillStyle = "rgba(50, 50, 50, 1)";
+	    that.backGround.Context.fillRect(0, 0, that.width, that.height);
+	    that.NBStars = 0;
+
 		// Rocket
 		that.rocket = parent.data.rocket;
 	
@@ -61,9 +71,7 @@ function GamePresenter(parent)
 		that.rocket.position = new Point(0,0);
 		that.rocket.speed = new Point(0,0);
 		
-		that.rocketImage = that.parent.data.images.rocket1;
-		that.asteroidImage = that.parent.data.images.asteroid1;
-		that.satelliteImage = that.parent.data.images.satellite1;		
+		that.rocketImage = that.parent.data.images.rocket;
    	 	// 180m is the height of a standard rocket
 	}
 	
@@ -123,43 +131,65 @@ function GamePresenter(parent)
 			that.rocket.engine.leftOn = false;
 		}
 		
+		var aspectRatio = 180 / 100;
+		var rocketXinMeters = that.rocket.position.x;
+		var rocketXinPixels = rocketXinMeters / aspectRatio;
+
+		if (that.NBStars < that.MaxStars && ((++that.clock)%10) == 0) {
+		    var color = new RGBColor();
+		    that.stars.push(new Star(Math.random() * that.width * aspectRatio, Math.random() * that.height * aspectRatio + that.rocket.position.y + 1000, 0, 0, 0.1 + 3 * Math.random(), color));
+		    that.NBStars++;
+		}
+
+
+		for (var i = 0; i < that.stars.length; i++) {
+		    var star = that.stars[i];
+		    //star.Animate(that.rocket);
+		    if (star.Data.Position.y < that.rocket.position.y - 100 || star.Data.MustDie()) {
+		        that.stars.splice(i, 1);
+                that.NBStars--;
+            }
+		}
+
+
 		//that.rocket.engine.leftOn = true;
 		that.rocket.nextStep(that.parent.data.planet, timeStep);
 		that.displayStage(timeStep);
 		return -1;
 	}
 	
-	that.displayStage = function(timeStep)
+	that.displayStage = function(timeStep) 
 	{
-		that.context.fillStyle = "rgba(50, 50, 50, 1)";
-   	 	that.context.fillRect(0, 0, that.canvas.width, that.canvas.height);
+	    var aspectRatio = 180 / 100;
+	    var rocketXinMeters = that.rocket.position.x;
+	    var rocketXinPixels = rocketXinMeters / aspectRatio;
+
+	    that.context.fillStyle = "rgba(255, 255, 255, 1)";
+	    that.context.fillRect(0, 0, that.width, that.height);
+
+	    that.backGround.Context.fillStyle = "rgba(50, 50, 50, 0.5)";
+	    that.backGround.Context.fillRect(0, 0, that.width, that.height);
+	    
+	    for (var i = 0; i < that.stars.length; i++) {
+	        that.stars[i].Trace(that.backGround, that.rocket.position, aspectRatio);
+	    }
+
+	    that.context.drawImage(that.backGround.Canvas, 0, 0);
+	    
+   	 	//that.context.fillRect(0, 0, that.canvas.width, that.canvas.height);
    	 	
    	 	
-   	 	that.rocketImage.style.height = "150px";
-		that.rocketImage.style.width = "80px";
+   	 	that.rocketImage.style.height = "100px";
+		that.rocketImage.style.width = "60px";
    	 	
-   	 	that.asteroidImage.style.height = "150px";
-		that.asteroidImage.style.width = "150px";
-		
-		that.satelliteImage.style.height = "150px";
-		that.satelliteImage.style.width = "150px";
+   	 	//var aspectRatio = 180 / that.rocketImage.height; 
    	 	
-   	 	//var aspectRatio = 180 / that.rocketImage.height;
-   	 	var aspectRatio = 180 / 100;
-   	 	var rocketXinMeters = that.rocket.position.x;
-   	 	var rocketXinPixels = rocketXinMeters / aspectRatio;
    	 	
    	 	that.context.fillStyle = "rgba(255, 255, 255, 1)";
-   	 	wrapText(that.context, "Speed = " + that.rocket.speed.x, 0, 30, 100, 20);
-   	 	wrapText(that.context, "Acceleration = " + that.rocket.accelereation.x, 0, 60, 100, 20);   
-   	 	wrapText(that.context, "Time Step = " + timeStep, 0, 90, 100, 20);   	
+   	 	wrapText(that.context, "height = " + that.rocket.position.y, 0, 30, 100, 20);
+   	 	//wrapText(that.context, "star = " + that.stars[0].Data.Position.x + " " + that.stars[0].Data.Position.y, 0, 60, 100, 20);
    	 	console.log(timeStep); 	
-   	 	that.context.drawImage(that.rocketImage, rocketXinPixels + that.canvas.width/2 - 40, that.canvas.height - 170, 80, 150);
-   	 	
-   	 	that.context.drawImage(that.asteroidImage, 100, 100, 150, 150); 
-   	 	that.context.drawImage(that.satelliteImage, 400, 200, 150, 150); 
-   	 	
-   	 	   	 	//that.rocketImage.onload = function()
+   	 	that.backGround.Context.drawImage(that.rocketImage, rocketXinPixels + that.canvas.width/2 - 30, that.canvas.height - 100, 60, 100);   	 	//that.rocketImage.onload = function()
     	//{
     		
   		//}
---
+++ js/Star.js
@@ -0,0 +1,73 @@
+﻿
+function Particule(iX, iY) {
+    var me = this;
+    this.Position = new Point(iX, iY);
+    this.Vitesse = new Point(0, 0);
+    this.Acceleration = new Point(0, 0);
+    this.Life = 0; 
+    this.LifeTime = 0; //Means Infinite
+    this.size = 10;
+    this.MustDie = function () {
+        if (me.LifeTime == 0)
+            return true;
+        return false;
+    }
+}
+
+function Star(iX, iY, sX, sY, distance, color) { //partial class Particule
+    var me = this;
+    this.Data = new Particule(iX, iY);
+    this.Type = "Star";
+    this.Data.LifeTime = 1000 * distance;
+    this.Data.Vitesse.x = sX / distance / 5;
+    this.Data.Vitesse.y = sY / distance / 5;
+    this.Data.size = 5 / distance;
+    this.Data.distance = distance;
+    this.Prerendered = document.createElement("canvas");
+    this.Prerendered.width = 2 * this.Data.size + 1;
+    this.Prerendered.height = 2 * this.Data.size + 1;
+    var ctx = this.Prerendered.getContext("2d");
+    ctx.translate(Math.round(this.Data.size), Math.round(this.Data.size));
+    var degrade = ctx.createRadialGradient(0, 0, 0, 0, 0, this.Data.size);
+    degrade.addColorStop(0, "rgba(255,255,255,1)");
+    degrade.addColorStop(0.5, "rgba(" + color.R + "," + color.G + "," + color.B + ",0)");
+    ctx.beginPath();
+
+    for (var i = 0; i < 2; i += 0.25) {
+        ctx.moveTo(0, 0);
+        ctx.lineTo(this.Data.size * Math.cos(i * Math.PI), this.Data.size * Math.sin(i * Math.PI));
+    }
+    ctx.closePath();
+    ctx.strokeStyle = degrade;
+    ctx.stroke();
+
+
+    this.Trace = function (ctx , p , ration) {
+        //ctx.globalAlpha = Math.sin(Math.PI * this.Data.Life / this.Data.LifeTime);
+        var newPoisition = new Point(this.Data.Position.x, this.Data.Position.y);
+        newPoisition.sub(p);
+        newPoisition.scalarDivide(ration);
+        ctx.Context.drawImage(this.Prerendered, newPoisition.x , ctx.Canvas.height - newPoisition.y);
+        ctx.Context.globalAlpha = 1;
+    };
+    this.Animate = function (p) {
+        var translation = new Point(p.x, p.y);
+        translation.scalarMultiply(1/distance);
+        me.Data.Position.add(translation);
+        this.Data.Life++; 
+    }
+}
+
+function PRCanvas(sX, sY) {
+    this.Canvas = document.createElement("canvas");
+    this.Canvas.width = sX;
+    this.Canvas.height = sY;
+    this.Context = this.Canvas.getContext("2d");
+}
+
+function RGBColor(ir, ig, ib) {
+    this.R = ir || Math.random() * 255 >> 0;
+    this.G = ig || Math.random() * 255 >> 0;
+    this.B = ib || Math.random() * 255 >> 0;
+
+}
