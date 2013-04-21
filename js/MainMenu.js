867ba5e13921a9ca421a966f4f5ebb3a02a59443
Guillaume Leclerc <guillaume.leclerc.work@gmail.com>
2013-04-21 11:23:10 +0200

    add mainMenu

---
+++ js/MainMenu.js
@@ -0,0 +1,15 @@
+﻿
+
+function MainMenu(i_parent) {
+    var that = this;
+    that.parent = i_parent;
+
+
+    that.setUp = function () {
+        that.parent.data.context.drawImage(that.parent.data.images.MenuBG, 0, 0);
+    }
+
+
+
+
+}
